7592da00dada7da0c2c85dd963c950d56f660ac4
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2024-05-03 14:55:15 +0000

    feat(front): basic layout

---
+++ frontend/src/components/dashboard/listItems.tsx
@@ -0,0 +1,78 @@
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import ChurchIcon from '@mui/icons-material/Church';
+import HomeIcon from '@mui/icons-material/Home';
+import LayersIcon from '@mui/icons-material/Layers';
+import PeopleIcon from '@mui/icons-material/People';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import { useRouter } from 'next/navigation';
+import * as React from 'react';
+
+export function mainListItems() {
+    const router = useRouter();
+    return (
+        <React.Fragment>
+            <ListItemButton onClick={() => router.push('/')}>
+                <ListItemIcon>
+                    <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+            </ListItemButton>
+            <ListItemButton onClick={() => router.push('/routes/church')}>
+                <ListItemIcon>
+                    <ChurchIcon />
+                </ListItemIcon>
+                <ListItemText primary="Church" />
+            </ListItemButton>
+            <ListItemButton onClick={() => router.push('/routes/members')}>
+                <ListItemIcon>
+                    <PeopleIcon />
+                </ListItemIcon>
+                <ListItemText primary="Members" />
+            </ListItemButton>
+            <ListItemButton>
+                <ListItemIcon>
+                    <BarChartIcon />
+                </ListItemIcon>
+                <ListItemText primary="Reports" />
+            </ListItemButton>
+            <ListItemButton>
+                <ListItemIcon>
+                    <LayersIcon />
+                </ListItemIcon>
+                <ListItemText primary="Integrations" />
+            </ListItemButton>
+        </React.Fragment>
+    )
+}
+
+export function secondaryListItems() {
+    return (
+        <React.Fragment>
+            <ListSubheader component="div" inset>
+                Saved reports
+            </ListSubheader>
+            <ListItemButton>
+                <ListItemIcon>
+                    <AssignmentIcon />
+                </ListItemIcon>
+                <ListItemText primary="Current month" />
+            </ListItemButton>
+            <ListItemButton>
+                <ListItemIcon>
+                    <AssignmentIcon />
+                </ListItemIcon>
+                <ListItemText primary="Last quarter" />
+            </ListItemButton>
+            <ListItemButton>
+                <ListItemIcon>
+                    <AssignmentIcon />
+                </ListItemIcon>
+                <ListItemText primary="Year-end sale" />
+            </ListItemButton>
+        </React.Fragment>
+    )
+};
