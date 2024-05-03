54f0ac0a29d0fe4a4063dbbdd3c209badc29eb92
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2024-05-03 17:07:09 +0000

    feat(back): basic spring boot project structure

---
+++ backend/src/main/java/br/com/dgc/simplechurch/SimpleChurchApplication.java
@@ -0,0 +1,13 @@
+package br.com.dgc.simplechurch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChurchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChurchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/dgc/simplechurch/SimpleChurchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dgc.simplechurch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChurchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
