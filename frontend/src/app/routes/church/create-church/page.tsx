8d100a93641c49c014cfada2645eda0ea362b91c
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2024-05-03 17:01:02 +0000

    feat(front): basic church creation

--- frontend/src/app/routes/church/create-church/page.tsx
+++ frontend/src/app/routes/church/create-church/page.tsx
@@ -1,7 +1,56 @@
-export default function CreateChurch(){
-    return(
-        <>
-            <h1>CreateChurch</h1>
-        </>
+'use client'
+import { Cancel } from '@mui/icons-material';
+import SaveIcon from '@mui/icons-material/Save';
+import Box from '@mui/material/Box';
+import FormGroup from '@mui/material/FormGroup';
+import IconButton from '@mui/material/IconButton';
+import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { useRouter } from 'next/navigation';
+export default function CreateChurch() {
+    const router = useRouter();
+    return (
+        <FormGroup sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem'
+        }}>
+            <Typography sx={{
+                alignSelf: 'center'
+            }} typography={'h3'}>Create church</Typography>
+            <Box sx={{
+                display: 'flex',
+                gap: '1rem'
+            }}>
+                <InputLabel sx={{
+                    display: 'inline-block',
+                    lineHeight: '3rem'
+                }} htmlFor='name'>Name:</InputLabel>
+                <TextField id='name'></TextField>
+            </Box>
+            <Box sx={{
+                display: 'flex',
+                gap: '1rem'
+            }}>
+                <InputLabel sx={{
+                    display: 'inline-block',
+                    lineHeight: '3rem'
+                }} htmlFor='cnpj'>CNPJ:</InputLabel>
+                <TextField id='cnpj'></TextField>
+            </Box>
+            <Box sx={{
+                display: 'flex',
+                gap: '1rem',
+                alignSelf: 'center'
+            }}>
+                <IconButton color='success'>
+                    <SaveIcon />
+                </IconButton>
+                <IconButton color='error' onClick={() => router.push('/routes/church')}>
+                    <Cancel />
+                </IconButton>
+            </Box>
+        </FormGroup>
     )
 }
