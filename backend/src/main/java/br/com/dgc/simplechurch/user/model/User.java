eb00f2d1cec8981a914394602917dcf0c7749b98
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2024-05-08 14:11:01 +0000

    fix(back): fix lazy initiation with eager type

--- backend/src/main/java/br/com/dgc/simplechurch/user/model/User.java
+++ backend/src/main/java/br/com/dgc/simplechurch/user/model/User.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,7 +41,7 @@ public class User implements UserDetails {
     private boolean accountNonLocked;
     private boolean credentialsNonExpired;
     private boolean enabled;
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private Set<Role> roles;
 
     public User() {
